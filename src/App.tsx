3ee7e7348e1d2ffe727e97b2838c37d589ae656b
BurntIce16 <clayton.midgley@gmail.com>
2024-09-24 18:39:40 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { invoke } from "@tauri-apps/api/core";
+import "./App.css";
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  return (
+    <div className="container">
+      <h1>Submissive</h1>
+      <h2>Upload a video to start</h2>
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
